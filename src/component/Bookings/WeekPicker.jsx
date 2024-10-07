ec5cda21e1666a7422c047f5c9dfb7249ce137e1
leegeon4 <green3694@gmail.con>
2024-10-07 09:48:10 +0900

    #17 예약 조회 fetch bookings 가져오는 방법 변경

--- src/component/Bookings/WeekPicker.jsx
+++ src/component/Bookings/WeekPicker.jsx
@@ -1,42 +1,47 @@
-import {useReducer} from "react";
-import reducer from "./weekReducer.js";
-import getWeek from "./date-util.js";
-import {FaCalendarDay, FaChevronLeft, FaChevronRight, FaChevronUp} from "react-icons/fa";
+import {FaCalendarDay, FaChevronLeft, FaChevronRight,FaCalendarCheck} from "react-icons/fa";
+import {formatDate} from "../utils/date-utils.js";
+import {useRef} from "react";
 
-function  WeekPicker({date}){
-    // date 값으로 getWeek 함수를 통해 state(week) 를 초기화
-    const [week, dispatch] = useReducer(reducer,date,getWeek)
-    return(
+function WeekPicker({week,dispatch}){
+    //* go 버튼 추가 : useRef 새로이 사용한 훅
+    const textboxRef = useRef();
+
+    function goToDate () {
+        dispatch({
+            type: "SET_DATE",
+            payload: textboxRef.current.value
+        });
+    }
+
+    return (
         <div>
             <p className="date-picker">
                 <button
                     className="btn"
                     onClick={() => dispatch({type: "PREV_WEEK"})}>
-                    <span>PREV</span>
                     <FaChevronLeft/>
+                    <span>PREV</span>
                 </button>
-                <input type="date" defaultValue={formatDate(new Date())}
-                       onChange={(e) => dispatch({
-                           type: "SET_DATE",
-                           payload: e.target.value
-                       })}
-                />
-                {/* 입력값을 state(상태값) 관리하면....
-                    이 예제에서는 날짜 타입으로 변환되는 문자열이 아니면
-                    예상치 않은 결과로 보입니다.
-                    입력값을 state 관리는 기능에 맞는 경우에만 사용합니다.*/}
-                <input type="text" placeholder="yyyy-mm-dd"
-                       defaultValue={formatDate(new Date())}
-                       onChange={(e) => dispatch({
-                           type: "SET_DATE",
-                           payload: e.target.value
-                       })}
-                />
+                <span>
+                  <input
+                      type="text"
+                      ref={textboxRef}
+                      placeholder="YYYY-MM-DD"
+                      defaultValue={formatDate(new Date())}
+                  />
+                <button
+                    className="go btn"
+                    onClick={goToDate}
+                >
+                  <FaCalendarCheck/>
+                  <span>Go</span>
+                </button>
+              </span>
                 <button
                     className="btn"
                     onClick={() => dispatch({type: "TODAY"})}>
-                    <span>Today</span>
                     <FaCalendarDay/>
+                    <span>Today</span>
                 </button>
                 <button
                     className="btn"
@@ -46,20 +51,14 @@ function  WeekPicker({date}){
                 </button>
             </p>
             <p>
-                {/*   {week.start.toLocaleDateString()} ~ {week.end.toLocaleDateString()}*/}
+                {/*{week.start.toLocaleString().substring(0,13)}
+                ~ {week.end.toLocaleString().substring(0,13)}*/}
                 {formatDate(week.start)} ~ {formatDate(week.end)}
             </p>
         </div>
+
     )
 
-}
-
-function formatDate(date) {
-    const year = date.getFullYear()
-    const month= String(date.getMonth()).padStart(2,'0')
-    const day = String (date.getDate()).padStart(2,'0')
-
-    return [year, month, day].join ('-')
 }
 
 
