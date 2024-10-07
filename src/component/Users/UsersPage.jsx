2d8e878c9c182fe4534b4befa195b21731009d6b
leegeon4 <green3694@gmail.con>
2024-10-07 15:05:34 +0900

    #20 user detail 분리

--- src/component/Users/UsersPage.jsx
+++ src/component/Users/UsersPage.jsx
@@ -1,9 +1,21 @@
-function UsersPage(){
-    return(
-        <main className="users-page">
-            <h4>사용자 현황</h4>
-        </main>
-    )
-}
+import UsersList from "./UserList.jsx";
+import {useContext, useState} from "react";
+import UserDetails from "./UserDetails.jsx";
 
+
+function UsersPage () {
+    const [user, setUser] = useState(null);
+
+
+    return (
+        <main className="users-page">
+            {/* 선택한 user 를 형제 컴포넌트와 공유하기 위해
+            부모 컴포넌트 UserPage 가 user 상태를 선언했으므로
+            UserList 도 props 를 선언합니다.
+            */}
+            <UsersList user={user} setUser={setUser}/>
+            <UserDetails user={user}/>
+        </main>
+    );
+}
 export default UsersPage
